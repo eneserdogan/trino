600c9a4ea91d692633c083f39950784e8b976961
Emirhan_Gul <emirhan.gl@windowslive.com>
2018-10-27 14:02:36 +0300

    A extra unit test is added for language detection function.

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const expect 	= require('chai').expect;
 const Translate = require('@google-cloud/translate')({
     key: 'AIzaSyBTx3natq5Zb7BkuSsp2WeJp74Q-6PXFnY'
 });
+var assert = require('assert');
 
 describe('Trino Command Line Tools', function() {
     it('<text> => "Hello" <target> => "tr" <result> => "Merhaba"', function() {
@@ -9,4 +10,13 @@ describe('Trino Command Line Tools', function() {
             expect(results[0]).to.equal('Merhaba');
         })
     });
+});
+
+
+describe('Trino Command Line Detection Test', function() {
+    it('<text> => "Günaydın" detection result should be "tr"', function() {
+       Translate.detect('Günaydın').then((results) => {
+            assert.equal(results[0],'en','Detection of this word could not be found!');
+       })
+  });
 });
